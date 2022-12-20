82e50da17637efefe6c86eed9750c4a727a270f7
Ricky Marinsalda <rickymarinsalda@gmail.com>
2022-12-20 17:38:06 +0100

    A first implementation of the Document's entities in classes
    Created class PointOfInterest, Line, Disruption, Location, Route

---
+++ libCommon/libCommon/src/main/java/londonSafeTravel/schema/document/Disruption.java
@@ -0,0 +1,34 @@
+package londonSafeTravel.schema.document;
+
+import londonSafeTravel.schema.Location;
+
+import java.util.Collection;
+import java.util.Date;
+
+public class Disruption {
+
+    public class Update{
+        public Date start;
+        public Date end;
+        public String message;
+    }
+    public class Street{
+        public String name;
+        public Boolean closure;
+    }
+    public String id;
+    public enum Type{PUBLIC_TRANSPORT, ROAD};
+    public Date start;
+    public Date end;
+    public Location coordinates;
+    public Collection<Location> boundaries;
+
+    public String category;
+
+    public String subCategory;
+    public String severity;
+    public Collection<Update> updates;
+
+    public Collection<Street> streets;
+    public Boolean closure; // se stazione (0,1), se in street esiste una closure(0,1)
+}
--- libCommon/libCommon/src/main/java/londonSafeTravel/schema/document/PointOfInterest.java
+++ libCommon/libCommon/src/main/java/londonSafeTravel/schema/document/PointOfInterest.java
@@ -1,4 +1,11 @@
 package londonSafeTravel.schema.document;
 
 public class PointOfInterest {
+    public String id; //  POI's id
+    public String name;
+    public String type;
+    public long latitude;
+    public long longitude;
+
+
 }
