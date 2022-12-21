43857f7c39c628c0288c6f9e179cff4eb482c9e0
Ricky Marinsalda <rickymarinsalda@gmail.com>
2022-12-21 12:35:58 +0100

    Creation PointOfInterestDAO class
    - Definition of PointOfInterestDAO class:
       - created method printAll() as a debug method
       - created method query1(minLong,maxLong,minLat,maxLat) which implements the query who show POIs in a given area

---
+++ src/libCommon/src/main/java/londonSafeTravel/schema/document/PointOfInterestDAO.java
@@ -0,0 +1,69 @@
+package londonSafeTravel.schema.document;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+import com.mongodb.client.model.geojson.Polygon;
+import com.mongodb.client.model.geojson.Position;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+
+import static com.mongodb.client.model.Filters.geoWithin;
+
+public class PointOfInterestDAO {
+    private PointOfInterest poi = new PointOfInterest();
+    private ConnectionMongoDB connection = new ConnectionMongoDB();
+
+    private MongoDatabase db = connection.giveDB();
+    private MongoCollection collection = db.getCollection("PointOfInterest");
+
+    public void printAll() {
+
+        try (
+                MongoCursor<Document> cursor = collection.find().iterator()) {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        }
+
+    }
+
+    public static void main(String[] argv)
+    {
+        PointOfInterestDAO poiDAO = new PointOfInterestDAO();
+
+        poiDAO.query1(-1, 100, -1, 90).forEach(d -> {
+            System.out.println(d.toJson());
+        });
+    }
+
+
+    public Collection<Document> query1(double minLong, double maxLong, double minLat, double maxLat)
+    {
+        Polygon region = new Polygon(Arrays.asList(
+                new Position(minLong, minLat),
+                new Position(maxLong, minLat),
+                new Position(maxLong, maxLat),
+                new Position(minLong, maxLat),
+                new Position(minLong, minLat)
+        ));
+
+        Bson myMatch = geoWithin("coordinates", region);
+        ArrayList<Document> results = new ArrayList<>();
+        collection.find(myMatch)
+                .forEach(doc -> {
+                    results.add((Document) doc);
+                });
+        return results;
+    }
+
+
+
+
+}
