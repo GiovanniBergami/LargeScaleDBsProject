3317991bed8447b29b0b430d6c18a6d9c71d303c
dario <dariopag45@gmail.com>
2022-12-23 21:25:12 +0100

    autoformatting code

--- src/OSMImporter/src/main/java/londonSafeTravel/OSMImporter/StreamImporter.java
+++ src/OSMImporter/src/main/java/londonSafeTravel/OSMImporter/StreamImporter.java
@@ -25,66 +25,63 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class StreamImporter {
-    private static HashMap<Long,Point> map;
-
+    private static HashMap<Long, Point> map;
 
 
     public static void main(String[] argv) throws FileNotFoundException, XMLStreamException {
         XMLInputFactory factory = XMLInputFactory.newInstance();
         XMLStreamReader r = factory.createXMLStreamReader(new FileReader("greater-london-latest.osm"));
-        HashMap<Long, Point> map=new HashMap<Long, Point>();
-        ManageWay manageWay=new ManageWay("neo4j://localhost:7687", "neo4j", "pass");
-        ManagePoint managePoint=new ManagePoint("neo4j://localhost:7687", "neo4j", "pass");
-        for(r.next(); r.hasNext(); r.next())
-        {
-            if(! r.isStartElement())
+        HashMap<Long, Point> map = new HashMap<Long, Point>();
+        ManageWay manageWay = new ManageWay("neo4j://localhost:7687", "neo4j", "pass");
+        ManagePoint managePoint = new ManagePoint("neo4j://localhost:7687", "neo4j", "pass");
+        for (r.next(); r.hasNext(); r.next()) {
+            if (!r.isStartElement())
                 continue;
 
-            if(! r.hasName() || ! Objects.equals(r.getLocalName(), "node"))
+            if (!r.hasName() || !Objects.equals(r.getLocalName(), "node"))
                 continue;
 
             final String currentNamespace = r.getNamespaceURI();
             double lat = Double.parseDouble(r.getAttributeValue(currentNamespace, "lat"));
             double lon = Double.parseDouble(r.getAttributeValue(currentNamespace, "lon"));
-            long id=Long.parseLong(r.getAttributeValue(currentNamespace, "id"));
-            Point p=new Point(id, lat, lon);
-            map.put(id,p);
-            System.out.println("Inserito punto con id: "+id);
+            long id = Long.parseLong(r.getAttributeValue(currentNamespace, "id"));
+            Point p = new Point(id, lat, lon);
+            map.put(id, p);
+            System.out.println("Inserito punto con id: " + id);
             managePoint.addNode(p);
         }
         r = factory.createXMLStreamReader(new FileReader("greater-london-latest.osm"));
-        long id1,id2;
+        long id1, id2;
 
-        for(r.next(); r.hasNext(); r.next()) {
+        for (r.next(); r.hasNext(); r.next()) {
             if (!r.hasName())
                 continue;
             final String currentNamespace = r.getNamespaceURI();
-            if(Objects.equals(r.getLocalName(),"way")){
-                boolean endWay=false;
-                ArrayList<Long> locations=new ArrayList<>();
-                HashMap<String,String> tags= new HashMap<>();
-                for(r.next(); !(r.isEndElement() && Objects.equals(r.getLocalName(),"way")); r.next()){
-                    if(!r.isStartElement() || !r.hasName())
+            if (Objects.equals(r.getLocalName(), "way")) {
+                boolean endWay = false;
+                ArrayList<Long> locations = new ArrayList<>();
+                HashMap<String, String> tags = new HashMap<>();
+                for (r.next(); !(r.isEndElement() && Objects.equals(r.getLocalName(), "way")); r.next()) {
+                    if (!r.isStartElement() || !r.hasName())
                         continue;
                     final String currentNamespaceIn = r.getNamespaceURI();
-                    if(Objects.equals(r.getLocalName(),"nd")){
+                    if (Objects.equals(r.getLocalName(), "nd")) {
                         locations.add(Long.parseLong(r.getAttributeValue(currentNamespaceIn, "ref")));
-                    }
-                    else if(Objects.equals(r.getLocalName(),"tag")){
-                        tags.put(r.getAttributeValue(currentNamespaceIn, "k"),r.getAttributeValue(currentNamespaceIn, "v"));
+                    } else if (Objects.equals(r.getLocalName(), "tag")) {
+                        tags.put(r.getAttributeValue(currentNamespaceIn, "k"), r.getAttributeValue(currentNamespaceIn, "v"));
                     }
                 }
                 //System.out.println(locations.size()+" "+tags.get("highway")+" "+tags.get("maxspeed"));
-                if(!tags.containsKey("highway") || !Objects.equals(tags.get("highway"), "primary"))
+                if (!tags.containsKey("highway") || !Objects.equals(tags.get("highway"), "primary"))
                     continue;
 
-                for(int i = 1; i<locations.size(); i++){
-                    Point p1 = map.get(locations.get(i-1));
+                for (int i = 1; i < locations.size(); i++) {
+                    Point p1 = map.get(locations.get(i - 1));
                     Point p2 = map.get(locations.get(i));
-                    Way w=new Way();
-                    w.name=tags.get("name");
-                    w.p1=p1;
-                    w.p2=p2;
+                    Way w = new Way();
+                    w.name = tags.get("name");
+                    w.p1 = p1;
+                    w.p2 = p2;
                     manageWay.addWay(w);
                 }
             }
