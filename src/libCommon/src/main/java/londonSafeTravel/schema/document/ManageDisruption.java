06024056a175d760892ea6d13f189147293944ac
Ricky Marinsalda <rickymarinsalda@gmail.com>
2022-12-23 16:30:00 +0100

    ManageDisruption skeleton

---
+++ src/libCommon/src/main/java/londonSafeTravel/schema/document/ManageDisruption.java
@@ -0,0 +1,16 @@
+package londonSafeTravel.schema.document;
+
+public class ManageDisruption {
+
+    /**
+     * @return a disruption or null if not exists
+     *
+    */
+    public Disruption get(String id){
+        return null;
+    }
+
+    public void set(Disruption d){
+
+    }
+}
