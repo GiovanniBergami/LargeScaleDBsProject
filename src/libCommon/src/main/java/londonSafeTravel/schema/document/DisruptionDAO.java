2bca4229bd80b2d50de56f184abed40a5642d1eb
Ricky Marinsalda <rickymarinsalda@gmail.com>
2022-12-23 14:58:30 +0100

    Query2 completed
    Find the most common disruption in a given area for each severity

--- src/libCommon/src/main/java/londonSafeTravel/schema/document/DisruptionDAO.java
+++ src/libCommon/src/main/java/londonSafeTravel/schema/document/DisruptionDAO.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Accumulators;
 import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Sorts;
 import com.mongodb.client.model.geojson.Polygon;
 import com.mongodb.client.model.geojson.Position;
 
@@ -14,13 +15,13 @@ import org.bson.conversions.Bson;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
-import static com.mongodb.client.model.Accumulators.addToSet;
-import static com.mongodb.client.model.Accumulators.avg;
+
 import static com.mongodb.client.model.Aggregates.*;
 import static com.mongodb.client.model.Filters.geoWithin;
 import static com.mongodb.client.model.Projections.*;
-import static com.mongodb.client.model.Sorts.descending;
+
 
 
 
@@ -41,8 +42,7 @@ public class DisruptionDAO {
     }
 
 
-    public static void main(String[] argv)
-    {
+    public static void main(String[] argv) {
         DisruptionDAO disDAO = new DisruptionDAO();
 
         disDAO.query2(-1, 100, -1, 90).forEach(d -> {
@@ -50,14 +50,14 @@ public class DisruptionDAO {
         });
     }
 
-    /* Find the most common disruption in a given area (per ogni severity) */
+    /* Find the most common disruption in a given area (for each severity) */
 
     /*
-        Medium:
-            Broken traffic light 10
+        Moderate:
+            Broken traffic light
             ...
-        High:
-            Collision 20
+        Minimal:
+            Collision
      */
 
 
@@ -65,7 +65,7 @@ public class DisruptionDAO {
         This function will return a list witch contains the most common disruption in order to severity
     */
 
-    public Collection<Document> query2(double minLong, double maxLong, double minLat, double maxLat){
+    public Collection<Document> query2(double minLong, double maxLong, double minLat, double maxLat) {
         Polygon region = new Polygon(Arrays.asList(
                 new Position(minLong, minLat),
                 new Position(maxLong, minLat),
@@ -75,44 +75,38 @@ public class DisruptionDAO {
         ));
 
         Bson inSquare = geoWithin("coordinates", region);
-        /*
-        Bson group1 = new Document("$group", new Document("_id", new Document("category", "$category")
-                .append("severity", "$severity"))
-                .append("nCategory", new Document("$sum",1L)));
-        Bson project1 = project(fields(excludeId(), computed("category", "$_id.category"),
-                computed("severity", "$_id.severity"), computed("nCategory", "$nCategory")));
-        Bson group2 = group("$severity",Accumulators.sum("nCategory", 1L));
-        Bson sort = sort(descending("nCategory"));
-        Bson project2 = project(fields(excludeId(), computed("severity", "$_id"), include("nCategory")));
-        */
-
-        Bson group1 = new Document("$group", new Document("_id", new Document("category", "$category").append("severity", "$severity"))
-                .append("nDisruptions", new Document("$sum", 1L))); // per ogni categoria mi conta quante disruption ci sono
-        Bson group2 = group("$_id.severity", Accumulators.sum("numCategory",1L ), addToSet("category", "$_id.category"));
-        Bson sort = sort(descending("numCategory"));
-        Bson project = project(fields(excludeId(), computed("category", "$_id"), include("category", "numCategory")));
 
 
-        /*Bson group = group("$severity", Accumulators.sum("count", 1L), Accumulators.push("category", "$category"));
-        Bson sort = sort(descending("count"));
-        //Bson project = project(fields(excludeId(),include("count", "name")));
-        Collection results = collection.aggregate(Arrays.asList(Aggregates.match(inSquare),group, sort)).into(new ArrayList<>());
-*/
-        ArrayList<Document> results = new ArrayList<>();
-        collection.aggregate(Arrays.asList(group1, group2,sort, project))
-                .forEach(doc -> {
-                    results.add((Document) doc);
-                });
+        // Create the group stage
+        Bson groupStage = Aggregates.group(
+                "$severity",
+                Accumulators.sum("count", 1),
+                Accumulators.first("type", "$category")
+        );
+        // Create the sort stage
+        Bson sortStage = Aggregates.sort(Sorts.descending("count"));
 
-        return results;
+        // Projèct
+
+        Bson project = project(fields(excludeId(),include("_id", "type")));
+
+        // Combine the stages into a pipeline
+        List<Bson> pipeline = Arrays.asList(Aggregates.match(inSquare), groupStage, sortStage,project);
+
+        // Execute the aggregation
+        Collection<Document> result = collection.aggregate(pipeline).into(new ArrayList<>());
+
+        return result;
+
     }
+
+
+
 
     /*
     Per ogni linea trovare per ogni giorno della settimana il numero di closures e la probabilità
     che essa sia coinvolta in una closure dove 1 = evento certo, 0 = evento impossibile
      */
-
-
 
 
 }
