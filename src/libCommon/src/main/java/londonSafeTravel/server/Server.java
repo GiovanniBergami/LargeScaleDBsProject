8f0d89e475becdf16fcfbce98e2a228456d09027
dario <dariopag45@gmail.com>
2023-01-06 10:07:22 +0100

    implemented a Server demo

---
+++ src/libCommon/src/main/java/londonSafeTravel/server/Server.java
@@ -0,0 +1,18 @@
+package londonSafeTravel.server;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class Server {
+    public static void main(String[] argv) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", 8080), 0);
+        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(8);
+
+        server.createContext("/query.json", new  QueryPointHandler());
+        server.setExecutor(threadPoolExecutor);
+        server.start();
+    }
+}
